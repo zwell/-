981ea293651a650f9fd0bc672ac23fb197d73aa6
weichang.zha <806248599@qq.com>
2017-05-11 18:15:22 +0800

    0511 pm

--- quickAdmin/resources/views/admin/booking/create.blade.php
+++ quickAdmin/resources/views/admin/booking/create.blade.php
@@ -77,7 +77,7 @@
 <div class="form-group">
     {!! Form::label('keep_datetime', '最晚保留时间', array('class'=>'col-sm-2 control-label')) !!}
     <div class="col-sm-10">
-        {!! Form::text('keep_datetime', old('keep_datetime'), array('id' => 'keep_datetime', 'class'=>'form-control')) !!}
+        {!! Form::text('keep_datetime', old('keep_datetime'), array('id' => 'keep_datetime', 'class'=>'form-control datetimepicker')) !!}
         
     </div>
 </div>
